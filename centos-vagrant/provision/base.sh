644b29de476a40e08a9a50fa625bfb537db72125
Kenneth Miles <kenneth.mmiles@gmail.com>
2018-04-28 02:41:14 -0700

    moving to gitlab

--- centos-vagrant/provision/base.sh
+++ centos-vagrant/provision/base.sh
@@ -2,7 +2,7 @@
 
 # install minimum packages
 sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install vim screen epel-release openssl-devel 
+sudo yum -y install vim screen wget bc cifs-utils epel-release openssl-devel livecd-tools
 
 # cache yum downloads on host machine
 sudo sed -i 's/\/var\/cache/\/Downloads/g' /etc/yum.conf
@@ -10,8 +10,9 @@ sudo sed -i 's/\/var\/cache/\/Downloads/g' /etc/yum.conf
 # disable selinux
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
-# install my dotfiles
+# dotfiles
 git clone https://github.com/kmmiles/dotfiles.git
 cd dotfiles && chmod +x copy.sh
-sudo -H -u vagrant ./copy.sh
+[ -d /vagrant ] && sudo -H -u vagrant ./copy.sh
+[ -d /root ] && sudo ./copy.sh
 cd .. && rm -rf dotfiles
--- debian-vagrant/provision/base.sh
+++ debian-vagrant/provision/base.sh
@@ -7,7 +7,7 @@ if [ -d $(dirname "$aptdir") ]; then
   sudo mv /tmp/apt.conf /etc/apt/apt.conf
 fi
 
-sudo apt-get install -y vim git screen
+sudo apt-get install -y vim git screen bc cifs-utils sshfs
 
 echo "alias ls='ls --color'" > /tmp/bashrc
 sudo mv /tmp/bashrc /etc
@@ -15,5 +15,6 @@ sudo mv /tmp/bashrc /etc
 # dotfiles
 git clone https://github.com/kmmiles/dotfiles.git
 cd dotfiles && chmod +x copy.sh
-sudo -H -u vagrant ./copy.sh
+[ -d /vagrant ] && sudo -H -u vagrant ./copy.sh
+[ -d /root ] && sudo ./copy.sh
 cd .. && rm -rf dotfiles
